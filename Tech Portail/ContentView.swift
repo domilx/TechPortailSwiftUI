eb790d948bc43bb3091ec21dcfacb81109044704
Domenico Valentino <domenico2727@gmail.com>
2022-04-10 16:00:38 -0400

    Start XCode Project

---
+++ Tech Portail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tech Portail
+//
+//  Created by Domenico Valentino on 2022-04-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tech Portail/Tech_PortailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_PortailApp.swift
+//  Tech Portail
+//
+//  Created by Domenico Valentino on 2022-04-10.
+//
+
+import SwiftUI
+
+@main
+struct Tech_PortailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
